e5b17f2bec7bb278a2f11268d13c78a93eb3643e
ldavidtl <torres_d44@yahoo.es>
2024-05-12 19:19:24 -0600

    add readme and script inital

--- script_sql.sql
+++ script_sql.sql
@@ -3,6 +3,8 @@ CREATE TABLE bdprueba.usuario (
 	usuario_id BIGINT auto_increment NOT NULL COMMENT 'primary key',
 	nombre varchar(100) NOT NULL,
 	password varchar(100) NULL,
+	email varchar(100) DEFAULT NULL,
+    username varchar(100) DEFAULT NULL,
 	CONSTRAINT usuario_PK PRIMARY KEY (usuario_id)
 )
 ENGINE=InnoDB
@@ -37,5 +39,9 @@ COLLATE=utf8mb4_0900_ai_ci
 AUTO_INCREMENT=1;
 
 ALTER TABLE bdprueba.purchase_order_detail ADD CONSTRAINT purchase_order_detail_FK FOREIGN KEY (order_id) REFERENCES bdprueba.purchase_order(order_id);
+INSERT INTO bdprueba.usuario
+( nombre, password, email, username)
+VALUES( 'David Torres', '$2a$10$1sRukf1ESzba4xZYFRqn7eCFu.lz0YpO5s/mlMu2yrQ8LGPscPHQy', 'aramandoalvarado@yahoo.com', 'admin');
+
 
 
